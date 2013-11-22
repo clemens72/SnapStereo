e4b6e7652c3cdcf0d0b12d9f2c4dc45baa558781
SwizzBeatz13 <bharatgogineni13@gmail.com>
2013-11-21 19:53:12 -0500

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,11 @@ SnapStereo::Application.routes.draw do
   match '/help',      to: 'static_pages#help',  via: 'get'
   match '/about',     to: 'static_pages#about', via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
- 
+  match "songs/upload", :as => "upload", via: 'post'
+  match "songs/downloads", :as => "downloads", via: 'post'
+  match "songs/delete", :as => "delete", via: 'post'
+  match '/songs',     to: 'songs#index',        via: 'get'
+
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
